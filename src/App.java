6907b3c7c0e8c86e6d76d5cb5448dd9595d07e74
LenaMazitova <0107lenam@gmail.com>
2024-07-17 10:22:42 +0200

    corrected all methods

--- src/App.java
+++ src/App.java
@@ -1,52 +1,74 @@
 import java.util.Comparator;
 
-
 public class App {
 
     public static void main(String[] args) throws Exception {
         CustomArrayList<String> strArr = new CustomArrayList<>();
         System.out.println();
+        System.out.println(strArr);
 
         strArr.add("a");
         strArr.add("b");
         strArr.add("c");
+        strArr.add(1, "d");
         System.out.println(strArr);
 
-        System.out.println(strArr.size());
+        strArr.removeByIndex(0);
+        System.out.println("After removing by index 0: " + strArr);
 
-        String str = strArr.removeWithReturn(0);
-        System.out.println(str);
+        String str = strArr.removeWithReturn(2);
+        System.out.println("Remove witn return by index 2: " + str);
         System.out.println();
 
-        CustomArrayList<String> ca2 = new CustomArrayList<>();
-        ca2.add("A");
-        ca2.add("B");
-        strArr.addAll(ca2);
-        System.out.println(strArr);
+        CustomArrayList<String> strArr2 = new CustomArrayList<>();
+        strArr2.add("A");
+        strArr2.add("B");
+        strArr2.add(null);
+        strArr.addAll(strArr2);
+        System.out.println("After adding all of items of another collection: " + strArr);
 
-        String str2 = strArr.get(2);
-        System.out.println(str2);
+        String strGet = strArr.get(2);
+        System.out.println(strGet);
+        System.out.println(strArr);
         strArr.clear();
-        System.out.println(strArr.isEmpty());
+        System.out.println(strArr.isEmpty() + " " + strArr);
 
         CustomArrayList<Integer> intArr = new CustomArrayList<>(0, 5);
         System.out.println();
         System.out.println(intArr);
 
         intArr.add(10);
-        System.out.println(intArr);
-
-        Integer[] arr = { 2, 9, 5 };
-        intArr.setArray(arr);
-        System.out.println(intArr);
-
         intArr.add(8);
         intArr.add(1);
         intArr.add(3);
         intArr.add(4);
-        intArr.add(8);
-        intArr.add(1);
-        intArr.add(3);
+        intArr.add(2);
+        intArr.add(5);
+        System.out.println(intArr);
+
+        intArr.add(3, 100);
+        System.out.println(intArr);
+
+        boolean fl = intArr.remove(Integer.valueOf(100));
+        System.out.println(fl + " " + intArr);
+
+        intArr.add(3, null);
+        System.out.println(intArr);
+
+        intArr.add(13, 101);
+        System.out.println(intArr);
+
+        intArr.add(14, 3);
+        Integer integer = intArr.get(14);
+        System.out.println("Index 14: " + integer + " Massiv: " + intArr);
+
+        intArr.add(15, null);
+        System.out.println(intArr);
+
+        intArr.deleteNulls();
+        System.out.println("After deleting nulls: " + intArr);
+
+        intArr.add(5, null);
         System.out.println(intArr);
 
         intArr.sort(Comparator.reverseOrder());
--- src/CustomArrayList.java
+++ src/CustomArrayList.java
@@ -12,11 +12,10 @@ public class CustomArrayList<E> extends AbstractList<E>
 
     private E[] array;
     private final int INITIAL_CAPACITY = 10;
-    private final Class<String> DEFAULT_TYPE = java.lang.String.class;
 
     @SuppressWarnings("unchecked")
     public CustomArrayList() {
-        array = (E[]) Array.newInstance(DEFAULT_TYPE, INITIAL_CAPACITY);
+        array = (E[]) new Object[INITIAL_CAPACITY];
     }
 
     @SuppressWarnings("unchecked")
@@ -25,31 +24,67 @@ public class CustomArrayList<E> extends AbstractList<E>
         Arrays.fill(array, defaultValue);
     }
 
-    public void setArray(E[] array) {
-        this.array = array;
+    public int size() {
+        int count = array.length;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] != null)
+                break;
+            if (array[i] == null) {
+                count--;
+            }
+        }
+        return count;
     }
 
-    public int size() {
+    public int getCapacity() {
         return array.length;
     }
 
+    private void checkArrayLength() {
+        if (size() == array.length) {
+            array = Arrays.copyOf(array, (array.length + (array.length >> 1 + 1)));
+        }
+    }
+
     public void add(int index, E element) {
+        int arraySize = size();
+        int arrayLength = array.length;
+
         try {
-            array[index] = element;
-        } catch (UnsupportedOperationException | IndexOutOfBoundsException e) {
+            if (index >= arrayLength) {
+                // If size != length there will not be increasing of capacity
+                checkArrayLength();
+                // Throws outofboundsexception when array is not filled
+                array[index] = element;
+            } else if (index == arrayLength - 1) {
+                if (array[index] == null)
+                    array[index] = element;
+                else {
+                    checkArrayLength();
+                    array[index] = element;
+                }
+            } else if (index <= arraySize && arraySize <= arrayLength) {
+                checkArrayLength();
+                arrayLength = array.length;
+                for (int i = arrayLength - 1; i > index; i--) {
+                    E dummy = array[i - 1];
+                    array[i] = dummy;
+                }
+                array[index] = element;
+            }
+        } catch (IndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public boolean add(E element) {
+        checkArrayLength();
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
                 array[index] = element;
                 return true;
             }
         }
-        array = Arrays.copyOf(array, (array.length + (array.length / 2 + 2)));
-        add(element);
         return false;
     }
 
@@ -68,10 +103,6 @@ public class CustomArrayList<E> extends AbstractList<E>
         }
     }
 
-    public E get(int index) {
-        return array[index];
-    }
-
     public boolean isEmpty() {
         for (E e : array) {
             if (e != null) {
@@ -81,32 +112,52 @@ public class CustomArrayList<E> extends AbstractList<E>
         return true;
     }
 
-    public boolean removeIndex(int index) {
-        if (index < array.length && index >= 0) {
+    public E get(int index) {
+        return array[index];
+    }
+
+    private void shiftForRemove(int index) {
+        if (index == array.length - 1)
             array[index] = null;
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+            if ((i + 1) == array.length - 1) {
+                array[i + 1] = null;
+            }
+        }
+    }
+
+    public boolean removeByIndex(int index) {
+        if (index < array.length && index >= 0) {
+            shiftForRemove(index);
             return true;
         }
         return false;
     }
 
     @SuppressWarnings("unchecked")
-    public E removeWithReturn(int index) {
+    public E removeWithReturn(int index) throws Exception {
+        boolean fl = false;
+        E dummy = (E) new Object();
+
         if (index < array.length && index >= 0) {
-            E dummy = array[index];
-            array[index] = null;
-            return dummy;
+            dummy = (E) array[index];
+            shiftForRemove(index);
+            fl = true;
         }
-        return (E) new Object();
+
+        if (fl == false) {
+            System.out.println("No such index");
+            throw new Exception("No such index");
+        }
+        return dummy;
     }
 
     public boolean remove(Object o) {
         int index = indexOf(o);
-        try {
-            if (index < array.length && index >= 0)
-                array[index] = null;
-                return true;
-        } catch (UnsupportedOperationException | IndexOutOfBoundsException e) {
-            System.err.println(e.getMessage());
+        if (index < array.length && index >= 0) {
+            shiftForRemove(index);
+            return true;
         }
         return false;
     }
@@ -125,17 +176,30 @@ public class CustomArrayList<E> extends AbstractList<E>
         return -1;
     }
 
+    public void deleteNulls() {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] == null && i != array.length - 1) {
+                int j = i + 1;
+                while (j < array.length - 1) {
+                    if (array[j] != null) {
+                        array[i] = array[j];
+                        array[j] = null;
+                        i++;
+                        j++;
+                    } else
+                        j++;
+                }
+            }
+        }
+    }
+
     public void sort(Comparator<? super E> c) {
         if (this.isEmpty())
             return;
+        this.deleteNulls();
 
         int leftIndex = 0;
-        int rightIndex = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != null) {
-                rightIndex += 1;
-            }
-        }
+        int rightIndex = size() - 1;
         quickSort(array, leftIndex, rightIndex, c);
     }
 
@@ -161,5 +225,11 @@ public class CustomArrayList<E> extends AbstractList<E>
             quickSort(array, leftIndex, j, c);
             quickSort(array, i, rightIndex, c);
         }
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList [array=" + Arrays.toString(array) + "\nnumber of elements. incl. inner nulls: " + size()
+                + "\ncapacity: " + array.length + "]";
     }
 }
